d2cf7b330c072aeb84d1a2d9aacdddafcc9bd2af
“KseGreb” <ksegreb@gmail.com>
2023-12-23 13:21:47 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// function oddNumbers(){
+
+//     const numbers = [1,2,3,4,5,6,7,8,9,10, 22, 23,24,27];
+//     const newNumber = numbers.filter(number => number%2 === 0)
+//             alert("Even numbers" + newNumber)
+// }
+// oddNumbers();
+
+
+
+const input = document.querySelector("#check");
+const par = document.querySelector("#par");
+
+input.addEventListener("click", result);
+
+function result(){
+    if (input.checked === true){
+        par.style.display = "block"
+    }
+    else{
+        par.style.display = "none"
+    }
+}
